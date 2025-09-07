b350846b4c6326b335ad7c5741bbd3b05117bd57
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-09-07 10:45:17 +0100

    add hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,25 +1,40 @@
 import React from 'react'
 import './Hero.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser,faClock,faFile } from "@fortawesome/free-solid-svg-icons";
 
 const Hero = () => {
   return (
     <div className='hero'>
-        <div>
+        <div className='hero-details'>
         <h2>Report an Incident</h2>
         <p>Be the voice of change and report incidents in the community</p>
-        <button>Report Now</button>
+        <button className='hero-button'>Report Now</button>
         </div>
 
         <div className='works'>
             <h2>How it Works</h2>
             <div className='works-details'>
 
+                <div className="register">
+                <FontAwesomeIcon icon={faUser} />
+                    <h3>Register</h3>
+                    <p>Create an account to get started</p>
+                </div>
+
+                <div className="register">
+                <FontAwesomeIcon icon={faFile} />
+                    <h3>Report</h3>
+                    <p>Submit a report about an incident</p>
+                </div>
+
+                <div className="register">
+                <FontAwesomeIcon icon={faClock} />
+                    <h3>Follow Up</h3>
+                    <p>Track the progess of your report </p>
+                </div>
             </div>
-
         </div>
-
-
-
     </div>
 
     
